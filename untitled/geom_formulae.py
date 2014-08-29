6e8fad4fe45989cefea33858d695c11b869600d3
ebrahima <asjed@aims.edu.gh>
2014-08-29 22:03:34 +0000

    test23

--- untitled/geom_formulae.py
+++ untitled/geom_formulae.py
@@ -1,23 +1,40 @@
 __author__ = 'asjed'
+
+
 import math
 from numpy import *
+
+
 def circle_area(radius):
     """
     calculate area of a circle
     :param radius: radius of a circle
-    :return:
+    :return:area of a circle(in square units of radius)
     """
     r2 = radius**2
     return math.pi*r2
 print(circle_area(5))
 
-def calculate_area_square(side):
-    r = side
-    return r*r
+def rectangle_area(length, width):
+    """
+    calculate area of a rectangle
+    :param length: length of a rectangle
+    :param width: width of a rectangle
+    :return:area of a rectangle
+    >>> rectangle_area(3,2)
+    6
+    """
+    return length*width
 
-print(calculate_area_square(2))
+print(rectangle_area(3, 2))
 
-def calculate_area_triangle(base, heigt):
+def triangle_area(base, heigt):
+    """
+    calculate area of a triangle
+    :param base: base of atringle
+    :param heigt:
+    :return:
+    """
     b = base
     h = heigt
     return 1/2*b*h
